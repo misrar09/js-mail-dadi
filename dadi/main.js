f0da7f4664e6146b2a4a6d6ecb2bbde7a9aaec0e
misrar09 <misrar09@gmail.com>
2023-10-31 23:15:32 +0100

    dadi with input

--- dadi/main.js
+++ dadi/main.js
@@ -1,43 +1,49 @@
 
-/* 
-
-TAKE THE INPUT NUMPBER FROM THE USER AND A RANDOM NUMBER FROM COMPUTER AND DETERMINE THE WINNER OF THE BASIS OF THE HIGHER NUMBER
-********************************************************************************************************************************
 
 const userInput = document.getElementById("user_input");
 const yourNumber = document.querySelector(".your_number")
 const computerNumber = document.querySelector(".computer_number")
 const btnEnter = document.getElementById("btn");
 const winner = document.getElementById("winner")
+winner.style.fontSize = "2rem"
+winner.style.color = "Green"
 
 btnEnter.addEventListener("click", function () {
     const userNumber = parseInt(userInput.value);
 
-    if (userNumber < 1 || userNumber > 6) {
+    if (isNaN(userNumber) || userNumber < 1 || userNumber > 6) {
         alert("please insert values from 1 to 6")
     }
     else {
 
         yourNumber.innerHTML = `Your Number: ${userNumber}`;
+
+        const randomNumber = Math.floor(Math.random() * (7 - 1) + 1)
+        computerNumber.innerHTML = `Computer's Number: ${randomNumber}`
+        
+        if (userNumber > randomNumber) {
+            winner.innerHTML = `You Won`
+        }
+        
+        else if (userNumber < randomNumber) {
+            winner.innerHTML = `Computer Won`
+        }
+        
+        else if (isNaN(userNumber)) {
+            winner.innerHTML = `computer won`
+        }
+        
+        else {
+            winner.innerHTML = `Draw`
+        }
+        
     }
 
-    const randomNumber = Math.floor(Math.random () * (7 - 1) + 1) 
-    computerNumber.innerHTML = `Computer's Number: ${randomNumber}`
+});
 
-    if(userNumber > randomNumber) {
-        winner.innerHTML = `You Won`
-    }
 
-    else{
-        winner.innerHTML = `Computer Won`
-    }
-    
 
-}) 
 
-*************************************************************************************************************************************
-
-*/
 
 
 
